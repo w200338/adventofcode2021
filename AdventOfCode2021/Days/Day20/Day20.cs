5a8f60fca8931b32eaee93f57fb0cb553c5c0ae7
Jurgen Coenegrachts <w200338@hotmail.com>
2021-12-20 08:06:31 +0100

    Find out that true evil does exist in the real world

--- AdventOfCode2021/Days/Day20/Day20.cs
+++ AdventOfCode2021/Days/Day20/Day20.cs
@@ -14,7 +14,6 @@
 
             string algorithm = inputLines[0];
             Dictionary<Vector2Int, bool> image = new Dictionary<Vector2Int, bool>();
-            RectangleInt bounds = new RectangleInt(Vector2Int.Zero - Vector2Int.One * 3, new Vector2Int(inputLines[3].Length, inputLines.Length - 2) + Vector2Int.One * 3);
 
             for (int y = 2; y < inputLines.Length; y++)
             {
@@ -23,6 +22,10 @@
                     image.Add(new Vector2Int(x, y - 2), inputLines[y][x] == '#');
                 }
             }
+
+            RectangleInt bounds = new RectangleInt(
+                new Vector2Int(image.Min(kvp => kvp.Key.X), image.Min(kvp => kvp.Key.Y)),
+                new Vector2Int(image.Max(kvp => kvp.Key.X), image.Max(kvp => kvp.Key.Y)));
 
             for (int step = 0; step < 2; step++)
             {
@@ -80,10 +83,9 @@
             return output;
         }
 
-        private int ReadPixel(Dictionary<Vector2Int, bool> image, Vector2Int position)
+        private int ReadPixel(Dictionary<Vector2Int, bool> image, Vector2Int position, bool background = false)
         {
             int output = 0;
-            //string bits = "";
             for (int i = -1; i <= 1; i++)
             {
                 for (int j = -1; j <= 1; j++)
@@ -93,10 +95,13 @@
                     {
                         output += isOn ? 1 : 0;
                     }
+                    else
+                    {
+                        output += background ? 1 : 0;
+                    }
                 }
             }
 
-            //var value = Convert.ToInt32(bits, 2);
             return output;
         }
 
@@ -106,28 +111,38 @@
 
             string algorithm = inputLines[0];
             Dictionary<Vector2Int, bool> image = new Dictionary<Vector2Int, bool>();
-            RectangleInt bounds = new RectangleInt(Vector2Int.Zero, new Vector2Int(inputLines[3].Length, inputLines.Length - 2));
 
             for (int y = 2; y < inputLines.Length; y++)
             {
-                for (int x = 0; x < inputLines[x].Length; x++)
+                for (int x = 0; x < inputLines[y].Length; x++)
                 {
                     image.Add(new Vector2Int(x, y - 2), inputLines[y][x] == '#');
                 }
             }
 
+            RectangleInt bounds = new RectangleInt(
+                new Vector2Int(image.Min(kvp => kvp.Key.X), image.Min(kvp => kvp.Key.Y)),
+                new Vector2Int(image.Max(kvp => kvp.Key.X), image.Max(kvp => kvp.Key.Y)));
+
+            //bounds.Position -= Vector2Int.One * 2;
+            //bounds.Size += Vector2Int.One * 4;
+
+            bool background = true;
             for (int step = 0; step < 50; step++)
             {
                 Dictionary<Vector2Int, bool> updatedPixels = new Dictionary<Vector2Int, bool>();
                 bounds.Position -= Vector2Int.One;
                 bounds.Size += Vector2Int.One * 2;
 
+                background = !background;
+
                 for (int y = bounds.Position.Y; y <= bounds.Size.Y; y++)
                 {
                     for (int x = bounds.Position.X; x <= bounds.Size.X; x++)
                     {
                         var position = new Vector2Int(x, y);
-                        int pixelIndex = ReadPixel(image, position);
+                        //int pixelIndex = ReadPixel(image, position, step % 2 == 1);
+                        int pixelIndex = ReadPixel(image, position, background);
 
                         updatedPixels.Add(position, algorithm[pixelIndex] == '#');
 
